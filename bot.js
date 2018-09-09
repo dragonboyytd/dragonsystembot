dacceab5e7ee837f974175858a51c439ca033518
dragonboyytd <42145713+dragonboyytd@users.noreply.github.com>
2018-09-09 18:56:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '/'
 
 client.on('ready', () => {
  client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
