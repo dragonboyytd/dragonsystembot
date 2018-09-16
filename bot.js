3a44628fe5b79c7b9e3b1d9248bd9192a3e89df2
dragonboyytd <42145713+dragonboyytd@users.noreply.github.com>
2018-09-16 02:21:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2878,10 +2878,27 @@ const youtube = new YouTube(GOOGLE_API_KEY);
 
 const queue = new Map();
 const PREFIX = '/';
-
 client.on('warn', console.warn);
 
 client.on('error', console.error);
+
+client.on('ready', () => { 
+console.log(`
+------------------------------------------------------
+> Logging in...
+------------------------------------------------------
+Logged in as ${client.user.tag}
+Working on ${client.guilds.size} servers!
+${client.channels.size} channels and ${client.users.size} users cached!
+I am logged in and ready to roll!
+LET'S GO!
+------------------------------------------------------
+-------------------------------------------------------
+------------------------------------------------------
+----------------------Bot's logs----------------------`);
+
+
+});
 
 
 
