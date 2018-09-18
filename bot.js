6f8deff4aed56efccd084ddbdc13b25d93bcd580
dragonboyytd <42145713+dragonboyytd@users.noreply.github.com>
2018-09-18 21:40:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,35 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+var prefix = "/";
+client.on('message', message => {
+
+    if (message.content === "mc") {
+                        if(!message.channel.guild) return message.reply(' هذا الامر فقط للسيرفرات !!');
+
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' ليس لديك صلاحيات');
+           message.channel.overwritePermissions(message.guild.id, {
+         SEND_MESSAGES: false
+
+           }).then(() => {
+               message.reply("تم تقفيل الشات ✅ ")
+           });
+             }
+if (message.content === "umc") {
+    if(!message.channel.guild) return message.reply(' هذا الامر فقط للسيرفرات !!');
+
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('ليس لديك صلاحيات');
+           message.channel.overwritePermissions(message.guild.id, {
+         SEND_MESSAGES: true
+
+           }).then(() => {
+               message.reply("تم فتح الشات✅")
+           });
+             }
 
 
+
+});
  
 
 
