89dcc89082c7d93bcf670e6afd5c3006be9bf085
dragonboyytd <42145713+dragonboyytd@users.noreply.github.com>
2018-09-20 15:48:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,183 +1,317 @@
 const Discord = require('discord.js');
-const HypixelAPI = require('hypixel-api')
-const moment = require('moment')
+
 const client = new Discord.Client();
 
-const args = process.argv.slice(2)
- 
-if (args.length < 2) {
-    console.log('Usage: node index.js <Discord bot token> <Hypixel API key>')
-    process.exit(0)
-}
- 
-const createRichEmbed = (title, description, color, image, footer, thumb) => {
-    let genEmbed = new Discord.RichEmbed({
-        title,
-        description
-    })
- 
-    genEmbed.setColor(color)
- 
-    if (image) genEmbed.setImage(image)
- 
-    if (footer) genEmbed.setFooter(footer)
- 
-    if (thumb) genEmbed.setThumbnail(thumb)
- 
-    return genEmbed
-}
- 
-const client = new Discord.Client()
-const HypixelClient = new HypixelAPI(args[1])
- 
-client.on('ready', () => {
-    client.user.setStatus('online')
-    client.user.setGame('!hycord')
- 
-    console.log('The bot has been initialized!')
- 
-    let installedGuilds = client.guilds.array().sort((a, b) => a.members.array().length > b.members.array().length ? 1 : -1)
- 
-    console.log('This bot is available on ' + installedGuilds.length + ' guilds:')
- 
-    let totalMembers = 0
- 
-    for (let i = 0; i < installedGuilds.length; i++) {
-        totalMembers += installedGuilds[i].memberCount
-        console.log(installedGuilds[i].name + ': ' + installedGuilds[i].memberCount + ' members')
-    }
- 
-    console.log('Total members: ' + totalMembers)
-})
- 
-client.on('message', async (message) => {
-    if (message.author.id === client.user.id) return
- 
-    if (!message.guild || !message.member) {
-        if (message.channel.recipient) {
-            message.channel.send('To talk to me, get my attention in servers using the `!hycord` command!')
-        }
-        return
-    }
- 
-    const messageContent = message.content
- 
-    if (messageContent.indexOf('!') !== 0) return
- 
-    if (!message.guild.me.hasPermission('ADMINISTRATOR')) {
-        console.log('Still need administrator permission in ' + message.guild.name)
-        await message.channel.send(createRichEmbed('Error', 'I need the **Administrator** permission to function!', '#E74C3C'))
-    }
- 
-    const commandComponents = messageContent.split('!')[1].split(' ')
-    const baseCommand = commandComponents[0].toLowerCase()
-    const commandArgs = (commandComponents.length > 1 ? commandComponents.slice(1) : [])
- 
-    switch (baseCommand) {
-        case 'hycord':
-            let helpRich = new Discord.RichEmbed()
- 
-            helpRich.setTitle('Hycord Bot Information')
- 
-            helpRich.setDescription('Hycord was created by [ethanent](https://ethanent.me)! Using the bot is simple!')
- 
-            helpRich.setColor('#FFE11A')
- 
-            helpRich.addField('!player <name>', 'Displays statistics for a player.')
- 
-            helpRich.addField('!guild <name>', 'Displays statistics for a Hypixel guild.')
- 
-            helpRich.setFooter('Hycord Bot | Created by ethanent', 'https://i.imgur.com/hFbNBr5.jpg')
- 
-            message.channel.send(helpRich)
-            break
-        case 'eval':
-            if (message.author.id === '249963809119272960') {
-                await message.channel.send('`' + eval(message.content.substring(6)) + '`')
-            }
-            break
-        case 'player':
-            if (commandArgs.length > 0) {
-                let hypixelPlayer
- 
-                message.channel.startTyping()
- 
-                try {
-                    hypixelPlayer = (await HypixelClient.getPlayer('name', commandArgs[0])).player
-                }
-                catch (err) {
-                    console.log(err)
-                    message.channel.stopTyping()
-                    message.channel.send('Hmm, that player doesn\'t seem to exist!')
-                    return
-                }
- 
-                let playerRich = new Discord.RichEmbed()
- 
-                playerRich.setThumbnail('https://crafatar.com/avatars/' + (hypixelPlayer.uuid || '') + '?size=100')
-                playerRich.setTitle('Hypixel Player: ' + hypixelPlayer.displayname)
-                playerRich.setURL('https://hypixel.net/player/' + hypixelPlayer.displayname + '/')
-                playerRich.setFooter('Hycord Bot | Created by ethanent', 'https://i.imgur.com/hFbNBr5.jpg')
-                playerRich.setColor('#30DB09')
- 
-                playerRich.addField('Rank', (hypixelPlayer.rank || hypixelPlayer.packageRank || hypixelPlayer.newPackageRank || 'None').toString().replace(/_/g, ' '), true)
-                playerRich.addField('Hypixel Level', hypixelPlayer.networkLevel || 'Not available', true)
-                playerRich.addField('Karma', hypixelPlayer.karma || 'Not available', true)
-                playerRich.addField('Client Version', hypixelPlayer.mcVersionRp || 'Not available', true)
-                playerRich.addField('First Login', hypixelPlayer.firstLogin ? moment(hypixelPlayer.firstLogin).calendar() : 'Not available', true)
-                playerRich.addField('Last Login', hypixelPlayer.lastLogin ? moment(hypixelPlayer.lastLogin).calendar() : 'Not available', true)
- 
-                let playerGuild
- 
-                let playerGuildID = (await HypixelClient.findGuild('member', hypixelPlayer.uuid)).guild
- 
-                if (playerGuildID) {
-                    playerGuild = (await HypixelClient.getGuild(playerGuildID)).guild
-                }
- 
-                playerRich.addField('Guild', (playerGuild ? '[' + playerGuild.name + ' [' + playerGuild.tag + ']' + '](https://hypixel.net/guilds/' + playerGuild._id + '/)' : 'None'))
- 
-                message.channel.stopTyping()
- 
-                message.channel.send(playerRich)
-            }
-            else {
-                message.channel.send('Usage: `!player <name>`')
-            }
-            break
-        case 'guild':
-            if (commandArgs.length > 0) {
-                message.channel.startTyping()
-                let targetGuild = await HypixelClient.findGuild('name', message.content.split('!' + baseCommand + ' ')[1])
-                message.channel.stopTyping()
-                if (targetGuild.guild === null) {
-                    message.channel.send('Hmm, that guild doesn\'t seem to exist!')
-                    return
-                }
- 
-                let guildData = (await HypixelClient.getGuild(targetGuild.guild)).guild
- 
-                let guildRich = new Discord.RichEmbed()
- 
-                guildRich.setThumbnail('https://hypixel.net/data/guild_banners/100x200/' + guildData._id + '.png')
-                guildRich.setTitle('Hypixel Guild: ' + guildData.name + ' [' + guildData.tag + ']')
-                guildRich.setFooter('Hycord Bot | Created by ethanent', 'https://i.imgur.com/hFbNBr5.jpg')
-                guildRich.setColor('#2DC7A1')
-                guildRich.setURL('https://hypixel.net/guilds/' + guildData._id + '/')
- 
-                guildRich.addField('Member Count', guildData.members.length, true)
-                guildRich.addField('Created', moment(guildData.created).calendar(), true)
-                guildRich.addField('Coins', guildData.coins, true)
- 
-                message.channel.send(guildRich)
-            }
-            else {
-                message.channel.send('Usage: `!guild <name>`')
-            }
-            break
-    }
-})
+const Util = require('discord.js');
 
+const getYoutubeID = require('get-youtube-id');
+
+const fetchVideoInfo = require('youtube-info');
+
+const YouTube = require('simple-youtube-api');
+
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+
+const queue = new Map();
+
+const ytdl = require('ytdl-core');
+
+const fs = require('fs');
+
+const gif = require("gif-search");
+
+const client = new Discord.Client({disableEveryone: true});
+
+const prefix = "//";
+/////////////////////////
+////////////////////////
+
+client.on('message', async msg =>{
+	if (msg.author.bot) return undefined;
+    if (!msg.content.startsWith(prefix)) return undefined;
+    
+    let args = msg.content.split(' ');
+
+	let command = msg.content.toLowerCase().split(" ")[0];
+	command = command.slice(prefix.length)
+
+    if(command === `ping`) {
+    let embed = new Discord.RichEmbed()
+    .setColor(3447003)
+    .setTitle("Pong!!")
+    .setDescription(`${client.ping} ms,`)
+    .setFooter(`Requested by | ${msg.author.tag}`);
+    msg.delete().catch(O_o=>{})
+    msg.channel.send(embed);
+    }
+});
+/////////////////////////
+////////////////////////
+//////////////////////
+client.on('message', async msg =>{
+	if (msg.author.bot) return undefined;
+    if (!msg.content.startsWith(prefix)) return undefined;
+    
+    let args = msg.content.split(' ');
+
+	let command = msg.content.toLowerCase().split(" ")[0];
+	command = command.slice(prefix.length)
+
+    if(command === `avatar`){
+	if(msg.channel.type === 'dm') return msg.channel.send("Nope Nope!! u can't use avatar command in DMs (:")
+        let mentions = msg.mentions.members.first()
+        if(!mentions) {
+          let sicon = msg.author.avatarURL
+          let embed = new Discord.RichEmbed()
+          .setImage(msg.author.avatarURL)
+          .setColor("#5074b3")
+          msg.channel.send({embed})
+        } else {
+          let sicon = mentions.user.avatarURL
+          let embed = new Discord.RichEmbed()
+          .setColor("#5074b3")
+          .setImage(sicon)
+          msg.channel.send({embed})
+        }
+    };
+});
+/////////////////////////
+////////////////////////
+//////////////////////
+/////////////////////////
+////////////////////////
+//////////////////////
+
+/////////////////////////
+////////////////////////
+//////////////////////
+/////////////////////////
+////////////////////////
+//////////////////////
+client.on('message', async msg => { 
+	if (msg.author.bot) return undefined;
+    if (!msg.content.startsWith(prefix)) return undefined;
+    
+    const args = msg.content.split(' ');
+	const searchString = args.slice(1).join(' ');
+    
+	const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
+	const serverQueue = queue.get(msg.guild.id);
+
+	let command = msg.content.toLowerCase().split(" ")[0];
+	command = command.slice(prefix.length)
+
+	if (command === `play`) {
+		const voiceChannel = msg.member.voiceChannel;
+        
+        if (!voiceChannel) return msg.channel.send("I can't find you in any voice channel!");
+        
+        const permissions = voiceChannel.permissionsFor(msg.client.user);
+        
+        if (!permissions.has('CONNECT')) {
+
+			return msg.channel.send("I don't have enough permissions to join your voice channel!");
+        }
+        
+		if (!permissions.has('SPEAK')) {
+
+			return msg.channel.send("I don't have enough permissions to speak in your voice channel!");
+		}
+
+		if (!permissions.has('EMBED_LINKS')) {
+
+			return msg.channel.sendMessage("I don't have enough permissions to insert a URLs!")
+		}
+
+		if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
+
+			const playlist = await youtube.getPlaylist(url);
+            const videos = await playlist.getVideos();
+            
+
+			for (const video of Object.values(videos)) {
+                
+                const video2 = await youtube.getVideoByID(video.id); 
+                await handleVideo(video2, msg, voiceChannel, true); 
+            }
+			return msg.channel.send(`**${playlist.title}**, Just added to the queue!`);
+		} else {
+
+			try {
+
+                var video = await youtube.getVideo(url);
+                
+			} catch (error) {
+				try {
+
+					var videos = await youtube.searchVideos(searchString, 5);
+					let index = 0;
+                    const embed1 = new Discord.RichEmbed()
+                    .setTitle(":mag_right:  YouTube Search Results :")
+                    .setDescription(`
+                    ${videos.map(video2 => `${++index}. **${video2.title}**`).join('\n')}`)
+                    
+					.setColor("#f7abab")
+					msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
+					
+/////////////////					
+					try {
+
+						var response = await msg.channel.awaitMessages(msg2 => msg2.content > 0 && msg2.content < 11, {
+							maxMatches: 1,
+							time: 15000,
+							errors: ['time']
+						});
+					} catch (err) {
+						console.error(err);
+						return msg.channel.send('No one respone a number!!');
+                    }
+                    
+					const videoIndex = parseInt(response.first().content);
+                    var video = await youtube.getVideoByID(videos[videoIndex - 1].id);
+                    
+				} catch (err) {
+
+					console.error(err);
+					return msg.channel.send("I didn't find any results!");
+				}
+			}
+
+            return handleVideo(video, msg, voiceChannel);
+            
+        }
+        
+	} else if (command === `skip`) {
+
+		if (!msg.member.voiceChannel) return msg.channel.send("You Must be in a Voice channel to Run the Music commands!");
+        if (!serverQueue) return msg.channel.send("There is no Queue to skip!!");
+
+		serverQueue.connection.dispatcher.end('Ok, skipped!');
+        return undefined;
+        
+	} else if (command === `stop`) {
+
+		if (!msg.member.voiceChannel) return msg.channel.send("You Must be in a Voice channel to Run the Music commands!");
+        if (!serverQueue) return msg.channel.send("There is no Queue to stop!!");
+        
+		serverQueue.songs = [];
+		serverQueue.connection.dispatcher.end('Ok, stopped & disconnected from your Voice channel');
+        return undefined;
+        
+	} else if (command === `vol`) {
+
+		if (!msg.member.voiceChannel) return msg.channel.send("You Must be in a Voice channel to Run the Music commands!");
+		if (!serverQueue) return msg.channel.send('You only can use this command while music is playing!');
+        if (!args[1]) return msg.channel.send(`The bot volume is **${serverQueue.volume}**`);
+        
+		serverQueue.volume = args[1];
+        serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1] / 50);
+        
+        return msg.channel.send(`Volume Now is **${args[1]}**`);
+
+	} else if (command === `np`) {
+
+		if (!serverQueue) return msg.channel.send('There is no Queue!');
+		const embedNP = new Discord.RichEmbed()
+	    .setDescription(`Now playing **${serverQueue.songs[0].title}**`)
+        return msg.channel.sendEmbed(embedNP);
+        
+	} else if (command === `queue`) {
+		
+		if (!serverQueue) return msg.channel.send('There is no Queue!!');
+		let index = 0;
+//	//	//
+		const embedqu = new Discord.RichEmbed()
+        .setTitle("The Queue Songs :")
+        .setDescription(`
+        ${serverQueue.songs.map(song => `${++index}. **${song.title}**`).join('\n')}
+**Now playing :** **${serverQueue.songs[0].title}**`)
+        .setColor("#f7abab")
+		return msg.channel.sendEmbed(embedqu);
+	} else if (command === `pause`) {
+		if (serverQueue && serverQueue.playing) {
+			serverQueue.playing = false;
+			serverQueue.connection.dispatcher.pause();
+			return msg.channel.send('Ok, paused');
+		}
+		return msg.channel.send('There is no Queue to Pause!');
+	} else if (command === "resume") {
+
+		if (serverQueue && !serverQueue.playing) {
+			serverQueue.playing = true;
+			serverQueue.connection.dispatcher.resume();
+            return msg.channel.send('Ok, resumed!');
+            
+		}
+		return msg.channel.send('Queue is empty!');
+	}
+
+	return undefined;
+});
+
+async function handleVideo(video, msg, voiceChannel, playlist = false) {
+	const serverQueue = queue.get(msg.guild.id);
+	console.log(video);
+	
+
+	const song = {
+		id: video.id,
+		title: Util.escapeMarkdown(video.title),
+		url: `https://www.youtube.com/watch?v=${video.id}`
+	};
+	if (!serverQueue) {
+		const queueConstruct = {
+			textChannel: msg.channel,
+			voiceChannel: voiceChannel,
+			connection: null,
+			songs: [],
+			volume: 5,
+			playing: true
+		};
+		queue.set(msg.guild.id, queueConstruct);
+
+		queueConstruct.songs.push(song);
+
+		try {
+			var connection = await voiceChannel.join();
+			queueConstruct.connection = connection;
+			play(msg.guild, queueConstruct.songs[0]);
+		} catch (error) {
+			console.error(`I could not join the voice channel: ${error}!`);
+			queue.delete(msg.guild.id);
+			return msg.channel.send(`Can't join this channel: ${error}!`);
+		}
+	} else {
+		serverQueue.songs.push(song);
+		console.log(serverQueue.songs);
+		if (playlist) return undefined;
+		else return msg.channel.send(`**${song.title}**, just added to the queue! `);
+	} 
+	return undefined;
+}
+
+function play(guild, song) {
+	const serverQueue = queue.get(guild.id);
+
+	if (!song) {
+		serverQueue.voiceChannel.leave();
+		queue.delete(guild.id);
+		return;
+	}
+	console.log(serverQueue.songs);
+
+	const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+		.on('end', reason => {
+			if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
+			else console.log(reason);
+			serverQueue.songs.shift();
+			play(guild, serverQueue.songs[0]);
+		})
+		.on('error', error => console.error(error));
+	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+
+	serverQueue.textChannel.send(`**${song.title}**, is now playing!`);
+}
 
 
 
