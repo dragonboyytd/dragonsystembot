f7882aabacc1fa48d95d4b6595904fbc22f9883e
dragonboyytd <42145713+dragonboyytd@users.noreply.github.com>
2018-09-20 15:50:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "//";
+const prefix = "/";
 /////////////////////////
 ////////////////////////
 
